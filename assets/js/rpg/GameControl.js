0b4d7604b4eba67f9e53e77fab4595b3ff65d4c8
YusufK-25 <yusufkhanswl@gmail.com>
2024-10-07 13:52:33 -0700

    huh

--- assets/js/rpg/GameControl.js
+++ assets/js/rpg/GameControl.js
@@ -1,6 +1,9 @@
 import GameEnv from './GameEnv.js';
-import Background from './Background.js';
 import Player from './Player.js';
+import Background from './Background.js';
+import Fish from './PlayerFish.js';
+import Turtle from './PlayerTurtle.js';
+import NPC from './NPC.js';
 
 /**
  * The GameControl object manages the game.
@@ -13,30 +16,64 @@ import Player from './Player.js';
  * making it easier to manage game states, handle events, and maintain the overall flow of the game.
  * 
  * @type {Object}
- * @property {Player} player - The player object.
+ * @property {Player} turtle - The player object.
+ * @property {Player} fish 
  * @property {function} start - Initialize game assets and start the game loop.
  * @property {function} gameLoop - The game loop.
  * @property {function} resize - Resize the canvas and player object when the window is resized.
  */
 const GameControl = {
 
-    start: function(assets = {}) {
-        GameEnv.create(); // Create the Game World, this is pre-requisite for all game objects.
-        this.background = new Background(assets.image || null);
-        this.player = new Player(assets.sprite || null);
+    start: function(gameLevel = {}) {
+        GameEnv.create();
+        for (let object of gameLevel.objects) {
+            GameEnv.gameObjects.push(new object.class(object.data));
+        }
+        // Start the game loop
         this.gameLoop();
+
+        // Add key event listener
+        window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    },
+
+    handleKeyDown: function(event) {
+        if (event.code === 'Space') {
+            this.checkProximityToNPC();
+        }
+    },
+
+    checkProximityToNPC: function() {
+        var player = GameEnv.gameObjects.find(obj => obj instanceof Fish); 
+        var npc = GameEnv.gameObjects.find(obj => obj instanceof NPC);
+
+        if (player && npc) {
+            var distance = Math.sqrt(
+                Math.pow(player.position.x - npc.position.x, 2) + Math.pow(player.position.y - npc.position.y, 2)
+            );
+
+            if (distance <= 100) {
+                this.showPrompt("Ribbit Ribbit");
+            }
+        }
+    },
+
+    showPrompt: function(message) {
+        alert(message);
     },
 
     gameLoop: function() {
         GameEnv.clear(); // Clear the canvas
-        this.background.draw();
-        this.player.update();
+        for (let object of GameEnv.gameObjects) {
+            object.update(); // Update the game objects
+        }
         requestAnimationFrame(this.gameLoop.bind(this));
     },
 
     resize: function() {
-        GameEnv.resize(); // Adapts the canvas to the new window size, ie a new Game World.
-        this.player.resize();
+        GameEnv.resize(); // Adapts the canvas to the new window size
+        for (let object of GameEnv.gameObjects) {
+            object.resize(); // Resize the game objects
+        }
     }
 };
 
