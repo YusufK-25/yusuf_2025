1852af55ac51b707d1f8b73f3aae1cd55fce42f5
YusufK-25 <yusufkhanswl@gmail.com>
2024-10-07 14:20:11 -0700

    frog

--- assets/js/rpg/Background.js
+++ assets/js/rpg/Background.js
@@ -12,7 +12,7 @@ export class Background {
 
     /* To draws are used to capture primary frame and wrap around to next frame
      * x to y is primary draw
-     * x + width to y is wrap around draw
+     * x + width to y is wrap around adraw
     */
     draw() {
         const ctx = GameEnv.ctx;
